8ff16fed8cd490e93271db3671084e1b894d7ab0
Gleb <keniomg77798@gmail.com>
2024-08-01 20:11:20 +0400

    1

---
+++ Assets/Scripts/Enemies/BearMover/BearMover.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
+public class BearMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _groundCheckPoint;
+    [SerializeField] private LayerMask _ground;
+
+    private bool _isWaiting;
+    private bool _isMovingRight;
+    private WaitForSeconds _waitForSeconds;
+
+    private void Start()
+    {
+        float turnDelay = 1;
+        _waitForSeconds = new WaitForSeconds(turnDelay);
+        _isMovingRight = true;
+    }
+
+    private void Update()
+    {
+        Move();        
+    }
+
+    private void Move()
+    {
+        Vector2 direction;
+
+        if (_isWaiting == false)
+        {
+            if (_isMovingRight)
+            {
+                direction = Vector2.right;
+            }
+            else
+            {
+                direction = Vector2.left;
+            }
+
+            transform.Translate(direction * _speed * Time.deltaTime);
+            CheckEdge();
+        }
+
+        ChangeAnimationDirection();
+    }
+
+    private void ChangeAnimationDirection()
+    {
+        Vector2 originalScale = transform.localScale;
+        Vector2 rightSideDirection = new(Mathf.Abs(originalScale.x), originalScale.y);
+        Vector2 leftSideDirection = new(-Mathf.Abs(originalScale.x), originalScale.y);
+
+        if (_isMovingRight)
+        {
+            transform.localScale = rightSideDirection;
+        }
+        else
+        {
+            transform.localScale = leftSideDirection;
+        }
+                
+        _animator.SetBool("isWaiting", _isWaiting);
+    }
+
+    private void CheckEdge()
+    {
+        Vector2 checkRayDirection;
+        float checkRayDistance = 0.5f;
+
+        if (_isMovingRight)
+        {
+            checkRayDirection = Vector2.right;
+        }
+        else
+        {
+            checkRayDirection= Vector2.left;
+        }
+
+        Vector2 groundCheckPosition = (Vector2)_groundCheckPoint.position + checkRayDirection * checkRayDistance;
+        RaycastHit2D groundCheckInfo = Physics2D.Raycast(groundCheckPosition, checkRayDirection, checkRayDistance, _ground);
+
+        if (groundCheckInfo.collider == false)
+        {
+            StartCoroutine(Turn());
+        }
+    }
+
+    private IEnumerator Turn()
+    {
+        _isWaiting = true;
+        yield return _waitForSeconds;
+        _isMovingRight = !_isMovingRight;
+        _isWaiting = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMover/Player/Mover.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private Transform _groundCheckPoint;
+    [SerializeField] private LayerMask _ground;
+    [SerializeField] private Animator _animator;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Run();
+        Jump();
+    }
+
+    private void Run()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 horizontalMovement = new(horizontal * _speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = horizontalMovement;
+
+        ChangeAnimationDirection(horizontal);
+    }
+
+    private void ChangeAnimationDirection(float getAxisInputValue)
+    {
+        Vector2 originalScale = transform.localScale;
+        Vector2 rightSideDirection = new(Mathf.Abs(originalScale.x), originalScale.y);
+        Vector2 leftSideDirection = new(-Mathf.Abs(originalScale.x), originalScale.y);
+
+        if (getAxisInputValue > 0)
+        {
+            transform.localScale = rightSideDirection;
+        }
+        else if (getAxisInputValue < 0)
+        {
+            transform.localScale = leftSideDirection;
+        }
+    }
+
+    private void Jump()
+    {
+        KeyCode jumpKey = KeyCode.Space;
+        CheckGrounded(out bool isGrounded);
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void CheckGrounded(out bool isGrounded)
+    {
+        float checkRadius = 0.1f;
+
+        isGrounded = Physics2D.OverlapCircle(_groundCheckPoint.position, checkRadius, _ground);
+
+        if (isGrounded == false)
+        {
+            if (_rigidbody.velocity.y > 0)
+            {
+                _animator.SetBool("isJump", true);
+                _animator.SetBool("isFall", false);
+            }
+            else if (_rigidbody.velocity.y < 0)
+            {
+                _animator.SetBool("isJump", false);
+                _animator.SetBool("isFall", true);
+            }
+        }
+        else
+        {
+            _animator.SetBool("isJump", false);
+            _animator.SetBool("isFall", false);
+        }
+    }
+}
